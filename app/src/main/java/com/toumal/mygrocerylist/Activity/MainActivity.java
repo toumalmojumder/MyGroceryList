6feb50c8df1707aaae7aac8c04d388a35f210f1b
Toumal Mojumder <32560686+toumalmojumder@users.noreply.github.com>
2020-01-18 21:22:00 +0600

    popup layout

--- app/src/main/java/com/toumal/mygrocerylist/Activity/MainActivity.java
+++ app/src/main/java/com/toumal/mygrocerylist/Activity/MainActivity.java
@@ -1,9 +1,10 @@
-package com.toumal.mygrocerylist;
+package com.toumal.mygrocerylist.Activity;
 
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.toumal.mygrocerylist.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
